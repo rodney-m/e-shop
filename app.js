89866da09fb371f891b540e200b8fe549bfb8c75
Rodney Mupanduki <rodneym@afrosoft.co.zw>
2021-11-11 12:15:54 -0800

    backend apis and database connection

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-
+const mongoose = require('mongoose')
+const cors = require('cors')
 require('dotenv/config');
 
-const api = process.env.API_URL;
+app.use(cors());
+app.options('*', cors() )
+
+
 
 // Middleware
 app.use(express.json());
 app.use(morgan('tiny'));
 
-app.get(`${api}/products`, (req, res) => {
-    const product = {
-        id: 1,
-        name: 'hair dresser',
-        image: 'some_url'
-    }
-    res.send(product)
-});
 
-app.post(`${api}/products`, (req, res) => {
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct);
-});
+//Routes
+const productsRoutes = require('./routes/products');
+const categoriesRoutes = require('./routes/categories');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
 
+const api = process.env.API_URL;
+
+app.use(`${api}/products`, productsRoutes)
+
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+
+// Database
+mongoose.connect(process.env.CONNECTION_STRING)
+.then(() => {
+    console.log('Database Connection is ready...');
+})
+.catch((err) => {
+    console.log(err)
+})
+
+// Server
 app.listen(3000, () => {
     console.log('server running on port 3000')
 });
