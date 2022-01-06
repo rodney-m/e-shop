77638d554aac4fd9c24f70839d9d7ee7b9c9cf1f
Rodney Mupanduki <rodneym@afrosoft.co.zw>
2022-01-06 10:00:47 -0800

    products routes

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,10 @@
 const {Product} = require('../models/product')
+const {Category} = require('../models/category')
 const express = require('express');
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
-    const productList = await Product.find();
+    const productList = await Product.find().select('name image');
 
     if(!productList) {
         res.status(500).json({success : false})
@@ -11,21 +12,40 @@ router.get(`/`, async (req, res) => {
     res.send(productList)
 });
 
-router.post(`/`, (req, res) => {
-    const product = new Product({
+router.get(`/:id`, async (req, res) => {
+    const product = await Product.findById(req.params.id).populate('category');
+
+    if(!product) {
+        res.status(500).json({success : false})
+    }
+    res.send(product)
+});
+
+router.post(`/`, async (req, res) => {
+    const category = await Category.findById(req.body.category);
+
+    if(!category) return res.status(400).send('Invalid Category');
+
+    let product = new Product({
         name : req.body.name,
+        description : req.body.description,
+        richDescription : req.body.richDescription,
         image : req.body.image,
-        countInStock: req.body.countInStock
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured
     })
 
-    product.save().then((createdProduct => {
-        res.status(201).json(createdProduct)
-    })).catch((err) => {
-        res.status(500).json({
-            error: err, 
-            success: false
-        })
-    })
+    product = await product.save()
+
+    if(!product)
+        return res.status(500).send('The product cannot be created')
+
+        return res.send(product);
 });
 
 module.exports = router;
