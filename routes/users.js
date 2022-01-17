923e4b343860fd5d3637deb57adeab652aa72b91
Rodney Mupanduki <rodneym@afrosoft.co.zw>
2022-01-17 16:58:01 +0200

    user routes

--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,69 @@
-const {User} = require('../models/user');
-const express = require('express');
+const { User } = require("../models/user");
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
 
-router.get(`/`, async (req, res) =>{
-    const userList = await User.find().select('-passwordHash');
+router.get(`/`, async (req, res) => {
+  const userList = await User.find().select("-passwordHash");
 
-    if(!userList) {
-        res.status(500).json({success: false})
-    } 
-    res.send(userList);
+  if (!userList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(userList);
 });
 
-router.get('/:id', async (req, res) =>   {
-    const user = await User.findById(req.params.id).select('-passwordHash');
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id).select("-passwordHash");
 
-    if(!user){
-        res.status(500).json({message: "The user with the given id was no found"})
-    }
-    res.status(200).send(user);
-})
+  if (!user) {
+    res
+      .status(500)
+      .json({ message: "The user with the given id was no found" });
+  }
+  res.status(200).send(user);
+});
 
-router.post('/', async (req, res) => {
-    let user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        color: req.body.color,
-        passwordHash: bcrypt.hashSync(req.body.password, 10),
-        phone: req.body.phone,
-        isAdmin: req.body.isAdmin,
-        apartment: req.body.isAdmin,
-        zip: req.body.zip,
-        city: req.body.city,
-        country: req.body.country,
-    })
+router.post("/", async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    color: req.body.color,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    apartment: req.body.isAdmin,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
 
-    user = await user.save();
+  user = await user.save();
 
-    if(!user)
-        return res.status(400).send('the user cannot be created!')
+  if (!user) return res.status(400).send("the user cannot be created!");
 
-    res.send(user);  
-})
+  res.send(user);
+});
 
-module.exports =router;
+router.put("/:id", async (req, res) => {
+  const user = await User.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+      color: req.body.color,
+      passwordHash: bcrypt.hashSync(req.body.password, 10),
+      phone: req.body.phone,
+      isAdmin: req.body.isAdmin,
+      apartment: req.body.isAdmin,
+      zip: req.body.zip,
+      city: req.body.city,
+      country: req.body.country,
+    },
+    { new: true }
+  );
+  if (!user) return res.status(400).send("the user cannot be created!");
+
+  res.send(user);
+});
+
+module.exports = router;
