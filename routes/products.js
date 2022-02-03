1af554a54c3a772a3662be0749e655e2db9af200
Rodney Mupanduki <rodneym@afrosoft.co.zw>
2022-02-03 01:39:52 -0800

    multer

--- routes/products.js
+++ routes/products.js
@@ -3,6 +3,23 @@ const {Category} = require('../models/category')
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/uploads')
+    },
+    filename: function (req, file, cb) {
+    //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    const fileName = file.originalname.split(' ').join('-')
+    //   cb(null, file.fieldname + '-' + uniqueSuffix)
+      cb(null, file.fileName + '-' + Date.mow())
+    }
+  })
+  
+  const uploadOptions = multer({ storage: storage })
+
+
 
 router.get(`/`, async (req, res) => {
     let filter = {};
@@ -28,15 +45,17 @@ router.get(`/:id`, async (req, res) => {
     res.send(product)
 });
 
-router.post(`/`, async (req, res) => {
+router.post(`/`, uploadOptions.single('image'), async (req, res) => {
     const category = await Category.findById(req.body.category);
     if(!category) return res.status(400).send('Invalid Category');
+
+    const fileName = req.file.filename
 
     let product = new Product({
         name : req.body.name,
         description : req.body.description,
         richDescription : req.body.richDescription,
-        image : req.body.image,
+        image : fileName,
         brand: req.body.brand,
         price: req.body.price,
         category: req.body.category,
